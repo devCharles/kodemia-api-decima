0b32250772d900e0e5dd0858722a3dac1e7c02d2
charles <charlesipn@gmail.com>
2021-01-26 09:56:40 -0600

    ADD cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 
 const express = require('express')
+const express = require('cors')
 const kodersRouter = require('./routes/koders')
 const authRouter = require('./routes/auth')
 const server = express()
 
+server.use(cors())
 server.use(express.json()) // Middleware
 server.use('/koders', kodersRouter)
 server.use('/auth', authRouter)
